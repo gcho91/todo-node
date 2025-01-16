27c351336e7d224636f2c01de0a5d52734da5058
Ria Cho <gcho91@gmail.com>
2025-01-16 11:45:42 -0600

    Add vercel.json

--- db.js
+++ db.js
@@ -1,13 +1,15 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
-  ssl: process.env.NODE_ENV === "production", // Use SSL in production environments
+  ssl: isProduction, // Simple SSL toggle
 });
 
 module.exports = pool;
